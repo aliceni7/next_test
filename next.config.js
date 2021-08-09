20307165e6c9c0d1002b606140a3466274d32782
Alice <ani00@stuy.edu>
2021-08-09 13:23:38 -0400

    a

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/next_test/' : '',
+}
+
+
