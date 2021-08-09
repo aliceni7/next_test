8a975bec5c404e951d63ad2ec49007fbb170be69
Alice Ni <55144138+aliceni7@users.noreply.github.com>
2021-08-09 13:37:20 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the prefix in production and not development.
-  assetPrefix: isProd ? '/next_test/' : '',
-}
-
-
